c013c4885e62efd94990cfe1c433c85661b25308
Adam <adam.grossman08@me.com>
2015-01-03 16:40:52 -0800

    expanding on django rest. json setup, just need to add nested comments

--- feed/admin.py
+++ feed/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from feed.models import Member, Group, Link, Comment
+from feed.models import Member, Group, Link, Comment, Vote
 
 admin.site.register(Member)
 admin.site.register(Group)
 admin.site.register(Link)
 admin.site.register(Comment)
+admin.site.register(Vote)
--- feed/v1/api/serializers.py
+++ feed/v1/api/serializers.py
@@ -3,22 +3,13 @@ from feed.models import Member, Group, Link, Comment, Vote
 
 
 class MemberSerializer(serializers.ModelSerializer):
-    posted = serializers.SerializerMethodField()
-    comments = serializers.SerializerMethodField()
-    votes = serializers.SerializerMethodField()
+    posted = serializers.SlugRelatedField(many=True, read_only=True, slug_field='title')
+    comments = serializers.StringRelatedField(many=True)
+    date_joined = serializers.DateTimeField(format="%m/%d/%Y")
 
     class Meta:
         model = Member
-        fields = ('id', 'username', 'first_name', 'last_name', 'email', 'profile_photo', 'bio', 'date_joined', 'posted', 'comments', 'votes',)
-
-    def get_posted(self, obj):
-        return Link.objects.filter(posted_user=obj)
-
-    def get_comments(self, obj):
-        return Comment.objects.filter(author=obj)
-
-    def get_votes(self, obj):
-        return Vote.objects.filter(voter=obj)
+        fields = ('id', 'username', 'first_name', 'last_name', 'profile_photo', 'bio', 'date_joined', 'posted', 'comments',)
 
 
 class GroupSerializer(serializers.ModelSerializer):
@@ -29,17 +20,40 @@ class GroupSerializer(serializers.ModelSerializer):
         fields = ('id', 'title', 'description', 'created_at', 'links')
 
     def get_links(self, obj):
-        return Link.objects.filter(group=obj)
+        # values list for list
+        return Link.objects.filter(group=obj).values_list('url', 'title', 'description', 'created_at', 'posted_user')
+        # values for dict
+        # return Link.objects.filter(group=obj).values('url', 'title', 'description', 'created_at', 'posted_user')
 
 
 class CommentSerializer(serializers.ModelSerializer):
+    author_name = serializers.SerializerMethodField()
+    # link = serializers.SerializerMethodField()
+
     class Meta:
         model = Comment
-        fields = ('id', 'created_at', 'body', 'link', 'author', 'parent',)
+        # fields = ('id', 'created_at', 'body', 'link', 'author_name', 'parent',)
+        fields = ('id', 'created_at', 'body', 'author_name', 'parent',)
+
+    def get_author_name(self, obj):
+        return obj.author.username
+
+    # def get_link(self, obj):
+    #     return obj.link.title
 
 
 class LinkSerializer(serializers.ModelSerializer):
+    comments = serializers.SerializerMethodField()
+    posted_by = serializers.SerializerMethodField()
+
     class Meta:
         model = Link
-        fields = ('id', 'title', 'url',)
+        fields = ('id', 'title', 'url', 'posted_by', 'comments',)
 
+    def get_posted_by(self, obj):
+        return obj.posted_user.username
+
+    def get_comments(self, obj):
+        all_comments = Comment.objects.filter(link=obj, parent__isnull=True)
+        serializer = CommentSerializer(all_comments, many=True)
+        return serializer.data
